3ee391c9d61d34f90c6d5ed57168b3ac162409bd
DontCloneBasil <niek05.middel@gmail.com>
2025-03-17 13:26:49 +0100

    added base version of page flipping and wireframe

--- project-Bookworm/Assets/Scripts/DisplayImage.cs
+++ project-Bookworm/Assets/Scripts/DisplayImage.cs
@@ -2,24 +2,40 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
 using System.Linq;
+using UnityEngine.InputSystem;
 
 public class DisplayImage : MonoBehaviour
 {
     [Header("reference")]
+    [SerializeField] private string FolderPath; //path to the folder 
+    [SerializeField] private string Name; // unique name accociated with the 'book'
     [SerializeField] private RawImage[] pages; //currently static path to the page raw image reference 
-    public int pageNumber = 1;
-    //[SerializeField] private string picturePath = $"Assets/OutputImages/page_1.png"; //temporary static path to png/jpg(up to you)
+    [SerializeField] private float pageNumber = 1;
+    
+    private float minPages = 1;
+    [SerializeField] private float maxPages; // max amount of pages
+    
 
     void Start()
     {
         LoadImage();
     }
 
+    private void OnFlip(InputValue inputValue)
+    {
+        float flip = pageNumber;
+        flip += inputValue.Get<Vector2>().x * pages.Length;
+        if (flip >= minPages && flip <= maxPages)
+        {
+            pageNumber = flip;
+            LoadImage();
+        }
+    }
     void LoadImage()
     {
         for (int i = 0; i < pages.Length; i++)
         {
-            string picPath = $"Assets/OutputImages/page_{pageNumber + i}.png";
+            string picPath = FolderPath + Name + $"page_{pageNumber + i}.jpeg";
             //checks if the path of the image is valid
             if (File.Exists(picPath))
             {
@@ -28,8 +44,10 @@ public class DisplayImage : MonoBehaviour
                 PageTexture.LoadImage(imageData); //gets the texture ready as a loaded version of an image
                 PageTexture.Apply(); //applies the image to the texture
                 pages[i].texture = PageTexture; //displays the texture
-
-
+            }
+            else 
+            {
+                pages[i].texture = null; // turns the page blank for books with uneven amount of pages;
             }
         }
     }
